ff887a726022ce2428114a4ed4669616c4017eae
Yusuf R Mohammed <93865623+Yrm5345@users.noreply.github.com>
2024-10-03 22:44:27 +0000

    Fixed problem 1

--- question1.php
+++ question1.php
@@ -5,41 +5,41 @@
 </head>
 <body>
     <h1>Math Calculator</h1>
-    <form action="calculate.php" method="post">
+    <form action="" method="post">
         <input type="number" name="num1" required>
         <input type="text" name="operator" required>
         <input type="number" name="num2" required>
         <button type="submit">Calculate</button>
     </form>
+
     <?php
-// Get the input values from the form
-$num1 = $_POST['num1'];
-$operator = $_POST['operator'];
-$num2 = $_POST['num2'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num1 = filter_var($_POST['num1'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $operator = filter_var($_POST['operator'], FILTER_SANITIZE_STRING);
+        $num2 = filter_var($_POST['num2'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 
-// Perform the calculation with operator precedence!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-switch ($operator) {
-    case '*':
-        $result = $num1 * $num2;
-        break;
-    case '/':
-        if ($num2 == 0) {
-            die("Division by zero is not allowed.");
+        switch ($operator) {
+            case '*':
+                $result = $num1 * $num2;
+                break;
+            case '/':
+                if ($num2 == 0) {
+                    die("Division by zero is not allowed.");
+                }
+                $result = $num1 / $num2;
+                break;
+            case '+':
+                $result = $num1 + $num2;
+                break;
+            case '-':
+                $result = $num1 - $num2;
+                break;
+            default:
+                die("Invalid operator.");
         }
-        $result = $num1 / $num2;
-        break;
-    case '+':
-        $result = $num1 + $num2;
-        break;
-    case '-':
-        $result = $num1 - $num2;
-        break;
-    default:
-        die("Invalid operator.");
-}
 
-// Display the result
-echo "<h1>Result: $result</h1>";
-?>
+        echo "<h1>Result: $result</h1>";
+    }
+    ?>
 </body>
 </html>
