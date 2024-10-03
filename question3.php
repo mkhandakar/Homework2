795ff5a69cabf3e55cdf4f725670ebc0853f5c42
Yusuf R Mohammed <93865623+Yrm5345@users.noreply.github.com>
2024-10-03 22:37:21 +0000

    Fixed problem 3

--- question3.php
+++ question3.php
@@ -4,40 +4,63 @@
     <title>Rounded Rectangle Bullseye</title>
 </head>
 <body>
+<pre>
 <?php
-$size = 7;
+$size = 7; 
 if ($size % 2 == 0 || $size < 5) {
     echo "Invalid size. Please enter an odd number greater than or equal to 5.";
 } else {
-    $middle = ceil($size / 2);
+    $middle = ceil($size / 2); 
+
+    
     for ($row = 1; $row <= $size; $row++) {
+      
         for ($col = 1; $col <= $size; $col++) {
+        
             if ($row == 1 && $col == 1) {
                 echo "/";
-            } elseif ($row == 1 && $col == $size) {
+            }
+          
+            elseif ($row == 1 && $col == $size) {
                 echo "\\";
-            } elseif ($row == $size && $col == 1) {
+            }
+           
+            elseif ($row == $size && $col == 1) {
                 echo "\\";
-            } elseif ($row == $size && $col == $size) {
+            }
+           
+            elseif ($row == $size && $col == $size) {
                 echo "/";
-            } elseif ($row == $middle && $col == $middle) {
+            }
+          
+            elseif ($row == $middle && $col == $middle) {
                 echo "+";
-            } elseif ($row == $middle) {
+            }
+            
+            elseif ($row == $middle) {
                 echo "-";
-            } elseif ($col == $middle) {
+            }
+           
+            elseif ($col == $middle) {
                 echo "|";
-            } elseif ($row == 1 || $row == $size) {
+            }
+          
+            elseif ($row == 1 || $row == $size) {
                 echo "-";
-            } elseif ($col == 1 || $col == $size) {
+            }
+        
+            elseif ($col == 1 || $col == $size) {
                 echo "|";
-            } else {
+            }
+          
+            else {
                 echo " ";
             }
         }
         echo "\n"; 
     }
 }
-
 ?>
+</pre>
 </body>
 </html>
